106dcbf0d7cc973f602377b75ad4b8d94608f1e8
sunho1999 <wmfrlek99@gmail.com>
2022-09-05 16:41:49 +0900

    1일 1커밋

---
+++ level2/땅따먹기/땅따먹기.py
@@ -0,0 +1,11 @@
+def solution(land):
+    answer = 0
+    N = len(land)
+    for i in range(0,N-1):
+        land[i+1][0] += max(land[i][1],land[i][2],land[i][3])
+        land[i+1][1] += max(land[i][0],land[i][2],land[i][3])
+        land[i+1][2] += max(land[i][0],land[i][1],land[i][3])
+        land[i+1][3] += max(land[i][0],land[i][1],land[i][2])
+    answer = max(land[N-1])
+
+    return answer
